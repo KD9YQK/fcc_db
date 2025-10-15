60e893bf0c1072fe89a3abf1f6ffdf96002b9422
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2025-10-15 00:17:15 -0500

    Create lookup.py

---
+++ lookup.py
@@ -0,0 +1,42 @@
+from main import get_callsign
+print("\nHam and GMRS Callsign Lookup\nWritten by KD9YQK\n")
+
+loop = True
+
+while loop:
+  callsign = input("Enter Callsign or (q)uit> ")
+  if callsign.lower() in ['q', 'quit']:
+    exit (0)
+  data = get_callsign(callsign.upper())
+  if len(data)< 1:
+    print("\nInvalid Callsign\n")
+    continue
+  line = "\n" + "-" * 55
+  print(line)
+  for k in data[0].keys():
+    line = k
+    line += " " * (10 - len(k))
+    try:
+      d = data[0][k]
+      if k == "service":
+        if d == "ZA":
+          d += " (GMRS)"
+        if d == "HA":
+          d += " (Ham)"
+      line += "| " + d
+      line += " " * (20 - len(d))
+    except:
+      line += "| " + " " * 20
+    try:
+      d = data[1][k]
+      if k == "service":
+        if d == "ZA":
+          d += " (GMRS)"
+        if d == "HA":
+          d += " (Ham)"
+      line += "| " + d
+    except:
+      line += "| "
+    print(line)
+  line = "\n" + "-" * 55 + "\n"
+  print(line)
